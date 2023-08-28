71e5980c67002a2e716fa0469aa2edd3c813ecec
yatish <ydipankar@gmail.com>
2023-08-27 22:25:15 -0400

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Yatish Gautam on 2023-08-27.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
